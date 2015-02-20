66f58de794e416000e6526076ba56f17a1a18faf
Jake Huggins <j.huggins@my.westminster.ac.uk>
2015-02-20 17:19:06 +0000

    Game logic update

--- app/src/main/java/com/w1441879/assignment1/GameLogic.java
+++ app/src/main/java/com/w1441879/assignment1/GameLogic.java
@@ -1,4 +1,121 @@
 package com.w1441879.assignment1;
 
+import java.util.Random;
+
 public class GameLogic {
+
+    Random rng = new Random();
+    private String[] operator ={"+", "-", "*", "/", "="};
+    String qEnd = " = ?";
+    int num1,num2,num3, num4, num5,num6;
+    private int difficulty = 0;
+    int[] numbers;
+    String[] qOperators;
+
+    GameLogic(int diff){
+        difficulty = diff;
+
+        switch(difficulty){
+            case 0:
+                numbers = new int[2];
+                break;
+            case 1:
+                int num1 = rng.nextInt(3-2 +1)+2; //max 3 ints, min 2 ints
+                System.out.println(num1);
+                numbers = new int[num1];
+                break;
+            case 2:
+                int num2 = rng.nextInt(4-2 +1)+2; //max 4 ints, min 2 ints
+                System.out.println(num2);
+                numbers = new int[num2];
+                break;
+            case 3:
+                int num3 = rng.nextInt(6-4 +1)+4; //max 6 ints, min 4 ints
+                System.out.println(num3);
+                numbers = new int[num3];
+                break;
+        }
+
+    }
+
+    public void createQuestion(){
+        System.out.println("getOperator");
+        getOperator();
+        System.out.println("getNum");
+        getNum();
+        System.out.println(" ");
+        for(int i = 0; i < numbers.length;i++){
+            System.out.print(numbers[i] + " " + qOperators[i] + " ");
+        }
+
+        Calculate();
+        /*int x =0;
+        int answer = Calculate(numbers[x], qOperators[x],numbers[x+1]);
+        x++;
+        answer =+ Calculate(answer, qOperators[x], numbers[x+1]);
+
+        System.out.println(answer);*/
+
+    }
+
+
+    private void getOperator(){
+        qOperators = new String[numbers.length];
+        int x = rng.nextInt(3-0 +1) +0;
+        //String op = operator[x];
+
+        for(int i = 0; i < qOperators.length-1;i++){
+            qOperators[i]=operator[rng.nextInt(3-0 +1) +0];
+            //qOperators[i]=operator[0];
+            System.out.println("index: "+ (i+1) + " " + qOperators[i]);
+        }
+
+        qOperators[qOperators.length-1] = "=";
+
+    }
+
+    private void getNum(){
+        for(int i = 0; i < numbers.length;i++){
+            numbers[i]=rng.nextInt(10 - 1 +1)+1; //max 10, min 1
+            System.out.println("index: "+ (i+1) + " " + numbers[i]);
+        }
+
+    }
+
+    private void Calculate(){
+        int tempTotal = 0;
+
+        tempTotal = Answer(numbers[0], qOperators[0], numbers[1]);
+
+        for(int i = 2; i < numbers.length; i++){
+            tempTotal = Answer(tempTotal, qOperators[i-1], numbers[i]);
+        }
+        System.out.println(tempTotal);
+    }
+
+    private int Answer(int num1, String op, int num2){
+        int answer = 0;
+
+        switch(op){
+            case "+":
+                answer = num1 + num2;
+                return answer;
+            case "-":
+                answer = num1 - num2;
+                return answer;
+            case "*":
+                answer = num1 * num2;
+                return answer;
+            case "/":
+                answer = num1 / num2;
+                return answer;
+            case "=":
+        }
+
+
+        return answer;
+    }
+
 }
+
+
